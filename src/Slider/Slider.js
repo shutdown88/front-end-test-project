2866f28b6defd0ca5c8ba1c9621d6b2121f8e93c
Daniele Galli <daniele.galli@eidosmedia.com>
2018-03-24 23:37:31 +0100

    Slider try

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Grid, Row, Col } from 'react-bootstrap';
 
 import './Slider.css';
 
@@ -18,6 +18,7 @@ const ControlIcon = ({ icon, altText }) => (
     </span>
 );
 
+// TODO create programmatically
 export default () => (
     <div className="Slider">
         <Carousel
@@ -26,59 +27,124 @@ export default () => (
             nextIcon={<ControlIcon icon={nextImg} altText="next" />}
         >
             <Carousel.Item>
-                <img alt="First" src={first} />
-                <Carousel.Caption>
-                    <h3>Powerful Extensions</h3>
-                    <p>
-                        This article is floated online with an aim to help you
-                        find the best dvd printing solution. Dvd printing is an
-                        important feature
-                    </p>
-                </Carousel.Caption>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                            <img
+                                className="carousel-intro"
+                                alt="First"
+                                src={first}
+                            />
+                        </Col>
+                        <Col md={4} mdOffset={1}>
+                            <div className="caption">
+                                <h3>Powerful Extensions</h3>
+                                <p>
+                                    This article is floated online with an aim
+                                    to help you find the best dvd printing
+                                    solution. Dvd printing is an important
+                                    feature
+                                </p>
+                                <p>
+                                    <a className="learn-more">Learn more</a>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </Carousel.Item>
             <Carousel.Item>
-                <img alt="second" src={second} />
-                <Carousel.Caption>
-                    <h3>Modern, User Friendly &amp; Powerful Interface</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua.
-                    </p>
-                </Carousel.Caption>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                            <img alt="second" src={second} />
+                        </Col>
+                        <Col md={4} mdOffset={1}>
+                            <div className="caption">
+                                <h3>
+                                    Modern, User Friendly &amp; Powerful
+                                    Interface
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <p>
+                                    <a className="learn-more">Learn more</a>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </Carousel.Item>
             <Carousel.Item>
-                <img alt="Third" src={third} />
-                <Carousel.Caption>
-                    <h3>Easy to costumize</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua.
-                    </p>
-                </Carousel.Caption>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                            <img alt="Third" src={third} />
+                        </Col>
+                        <Col md={4} mdOffset={1}>
+                            <div className="caption">
+                                <h3>Easy to costumize</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <p>
+                                    <a className="learn-more">Learn more</a>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </Carousel.Item>
             <Carousel.Item>
-                <img alt="Fourth" src={fourth} />
-                <Carousel.Caption>
-                    <h3>Elegant &amp; Perfect Design</h3>
-                    <p>
-                        Te sea deleniti suscipiantur deterruisset, eu quis
-                        detracto sapientem mea. An purto dicunt deserunt vix,
-                        eam te iudico tritani. Dicunt irure.
-                    </p>
-                </Carousel.Caption>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                            <img alt="Fourth" src={fourth} />
+                        </Col>
+                        <Col md={4} mdOffset={1}>
+                            <div className="caption">
+                                <h3>Elegant &amp; Perfect Design</h3>
+                                <p>
+                                    Te sea deleniti suscipiantur deterruisset,
+                                    eu quis detracto sapientem mea. An purto
+                                    dicunt deserunt vix, eam te iudico tritani.
+                                    Dicunt irure.
+                                </p>
+                                <p>
+                                    <a className="learn-more">Learn more</a>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </Carousel.Item>
             <Carousel.Item>
-                <img alt="Fifth" src={fifth} />
-                <Carousel.Caption>
-                    <h3>Clean &amp; Powerful Theme</h3>
-                    <p>
-                        Te sea deleniti suscipiantur deterruisset, eu quis
-                        detracto sapientem mea. An purto dicunt deserunt vix,
-                        eam te iudico tritani. Dicunt irure.
-                    </p>
-                </Carousel.Caption>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                            <img alt="Fifth" src={fifth} />
+                        </Col>
+                        <Col md={4} mdOffset={1}>
+                            <div className="caption">
+                                <h3>Clean &amp; Powerful Theme</h3>
+                                <p>
+                                    Te sea deleniti suscipiantur deterruisset,
+                                    eu quis detracto sapientem mea. An purto
+                                    dicunt deserunt vix, eam te iudico tritani.
+                                    Dicunt irure.
+                                </p>
+                                <p>
+                                    <a className="learn-more">Learn more</a>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </Carousel.Item>
         </Carousel>
     </div>
